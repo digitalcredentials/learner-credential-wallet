b4040b67844afd6dbf849bb639820f3f4772bd37
kylehovey <khoveyc@gmail.com>
2021-08-10 16:14:46 -0700

    Added stub for logout button

--- app/screens/SettingsScreen/SettingsScreen.tsx
+++ app/screens/SettingsScreen/SettingsScreen.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Header } from 'react-native-elements';
+import { Button } from 'react-native-elements';
+import { useDispatch } from 'react-redux';
 
-import styles from './SettingsScreen.style';
 import mixins from '../../styles/mixins';
+import { lock } from '../../store/slices/wallet';
 
-export default () => (
-  <>
-    <Header
-      centerComponent={{ text: 'Settings', style: mixins.headerTitle}}
-      containerStyle={mixins.headerContainer}
-    />
-    <View style={mixins.bodyContainer}>
-      <Text>Settings</Text>
-    </View>
-  </>
-);
+export default () => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <Header
+        centerComponent={{ text: 'Settings', style: mixins.headerTitle}}
+        containerStyle={mixins.headerContainer}
+      />
+      <View style={mixins.bodyContainer}>
+        <Text>Settings</Text>
+        <Button title="Lock Wallet" onPress={() => dispatch(lock())} />
+      </View>
+    </>
+  );
+};
