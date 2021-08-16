1695f85e722209ef0a30a05a24cde8adea2ded19
kylehovey <khoveyc@gmail.com>
2021-08-16 16:28:13 -0700

    Switched to conditional rendering for root route

--- App.tsx
+++ App.tsx
@@ -37,6 +37,7 @@ export default function App(): JSX.Element {
     }
   }, [walletStateInitialized]); 
 
+
   if (!fontsLoaded || !walletStateInitialized) {
     return <AppLoading />;
   }
--- app/navigation/HomeNavigation/HomeNavigation.tsx
+++ app/navigation/HomeNavigation/HomeNavigation.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import theme from '../../styles/theme';
 import { HomeScreen, ShareScreen, AddScreen, SettingsScreen } from '../../screens';
 import type { TabIconProps } from './HomeNavigation.d';
-import type { HomeNavigationProps } from '../AppNavigation/AppNavigation.d';
-import { WalletState, getAllCredentials } from '../../store/slices/wallet';
-import { RootState } from '../../store';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -18,29 +14,7 @@ const ShareTabIcon = ({ color }: TabIconProps) => <MaterialIcons name="share" co
 const AddTabIcon = ({ color }: TabIconProps) => <MaterialIcons name="add-circle" color={color} size={theme.iconSize} />;
 const SettingsTabIcon = ({ color }: TabIconProps) => <MaterialIcons name="settings" color={color} size={theme.iconSize} />;
 
-export default function HomeNavigation({ navigation }: HomeNavigationProps): JSX.Element {
-  const dispatch = useDispatch();
-  const {
-    isUnlocked,
-    isInitialized,
-  } = useSelector<RootState, WalletState>(({ wallet }) => wallet);
-
-  useEffect(() => {
-    if (isInitialized && !isUnlocked) {
-      navigation.navigate('LoginScreen');
-    }
-  }, [isUnlocked, isInitialized]);
-
-  useEffect(() => {
-    dispatch(getAllCredentials());
-  }, []);
-
-  // Prevent users from going back
-  useEffect(
-    () => navigation.addListener('beforeRemove', (e) => e.preventDefault()), 
-    [navigation],
-  );
-
+export default function HomeNavigation(): JSX.Element {
   return (
     <Tab.Navigator
       activeColor={theme.color.iconActive}
