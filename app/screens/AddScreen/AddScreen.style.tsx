2218b8d982e87244c93b148ec5de15df554f90ba
kylehovey <khoveyc@gmail.com>
2021-08-24 12:19:41 -0700

    Simplified credential add view (no longer need to have AddCredentialView component)

--- app/screens/AddScreen/AddScreen.style.tsx
+++ app/screens/AddScreen/AddScreen.style.tsx
@@ -1,8 +1,15 @@
 import { StyleSheet } from 'react-native';
 
+import mixins from '../../styles/mixins';
+
 export default StyleSheet.create({
   container: {
     padding: 16,
     flex: 1,
+  },
+  paragraph: {
+    ...mixins.paragraphText,
+    marginBottom: 24,
+    marginTop: 8,
   },
 });
--- app/screens/AddScreen/AddScreen.tsx
+++ app/screens/AddScreen/AddScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Header } from 'react-native-elements';
+import { View } from 'react-native';
+import { Header, Text, Button } from 'react-native-elements';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
+import theme from '../../styles/theme';
 import mixins from '../../styles/mixins';
-import AddCredentialView from '../../components/AddCredentialView/AddCredentialView';
-import styles from './AddScreen.style';
+import style from './AddScreen.style';
 import { AddScreenProps } from '../../navigation/AddCredentialNavigation/AddCredentialNavigation.d';
 
 export default function AddScreen({ navigation }: AddScreenProps): JSX.Element {
@@ -13,12 +15,40 @@ export default function AddScreen({ navigation }: AddScreenProps): JSX.Element {
         centerComponent={{ text: 'Add Credential', style: mixins.headerTitle}}
         containerStyle={mixins.headerContainer}
       />
-      <AddCredentialView
-        style={styles.container}
-        /* TODO: If we can turn this into a screen we don't have to pass these */
-        goToQR={() => navigation.navigate('QRScreen')}
-        goToImport={() => navigation.navigate('ImportScreen')}
-      />
+      <View style={style.container}>
+        <Text style={style.paragraph}>
+          To add credentials, follow an approved link from an issuer (most often a
+          University) or use the options below.
+        </Text>
+        <Button
+          title="Scan QR code"
+          buttonStyle={mixins.buttonIcon}
+          titleStyle={mixins.buttonIconTitle}
+          iconRight
+          onPress={() => navigation.navigate('QRScreen')}
+          icon={
+            <MaterialIcons
+              name="qr-code-scanner"
+              size={theme.iconSize}
+              color={theme.color.iconInactive}
+            />
+          }
+        />
+        <Button
+          title="Restore from a file"
+          buttonStyle={mixins.buttonIcon}
+          titleStyle={mixins.buttonIconTitle}
+          iconRight
+          onPress={() => navigation.navigate('ImportScreen')}
+          icon={
+            <MaterialCommunityIcons
+              name="file-upload"
+              size={theme.iconSize}
+              color={theme.color.iconInactive}
+            />
+          }
+        />
+      </View>
     </>
   );
 }
