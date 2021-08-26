13443f8ecc49a79f578b4502d21fc65dfb373066
kylehovey <khoveyc@gmail.com>
2021-08-26 14:18:01 -0700

    Added instructions for QR scanner and fixed the height

--- app/screens/QRScreen/QRScreen.styles.ts
+++ app/screens/QRScreen/QRScreen.styles.ts
@@ -1,10 +1,28 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
+import mixins from '../../styles/mixins';
 import theme from '../../styles/theme';
 
 export default StyleSheet.create({
+  scannerBody: {
+    flex: 1,
+    backgroundColor: theme.color.backgroundPrimary,
+  },
+  instructionContainer: {
+    height: 50,
+    backgroundColor: theme.color.backgroundPrimary,
+  },
+  instructionText: {
+    ...mixins.headerTitle,
+    fontFamily: theme.fontFamily.regular,
+    fontSize: 16,
+    lineHeight: 24,
+    flex: 0,
+  },
   cameraStyle: {
-    height: Dimensions.get('window').height,
+    flex: 1,
+    flexBasis: '80%',
+    height: '100%',
   },
   markerStyle: {
     borderColor: theme.color.textPrimary,
--- app/screens/QRScreen/QRScreen.tsx
+++ app/screens/QRScreen/QRScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Text } from 'react-native-elements';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 import { QRScreenProps } from '../../navigation/AddCredentialNavigation/AddCredentialNavigation.d';
@@ -7,12 +8,21 @@ import NavHeader from '../../components/NavHeader/NavHeader';
 import styles from './QRScreen.styles';
 
 export default function QRScreen({ navigation: { goBack }}: QRScreenProps): JSX.Element {
+  function Instructions(): JSX.Element {
+    return (
+      <Text style={styles.instructionText}>
+        Scan a QR code from your issuer to request your credentials.
+      </Text>
+    );
+  }
+
   return (
-    <View>
+    <View style={styles.scannerBody}>
       <NavHeader title="Scan QR" goBack={goBack} />
       <QRCodeScanner
         onRead={msg => console.log(msg)}
-        topViewStyle={styles.emptyContainer}
+        topContent={<Instructions />}
+        topViewStyle={styles.instructionContainer}
         bottomViewStyle={styles.emptyContainer}
         cameraStyle={styles.cameraStyle}
         markerStyle={styles.markerStyle}
