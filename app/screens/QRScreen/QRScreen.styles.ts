364e432da192295780e48f19ab58c0f28db2a84d
kylehovey <khoveyc@gmail.com>
2021-08-26 17:10:15 -0700

    Removed unused import

--- app/screens/QRScreen/QRScreen.styles.ts
+++ app/screens/QRScreen/QRScreen.styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import mixins from '../../styles/mixins';
 import theme from '../../styles/theme';
