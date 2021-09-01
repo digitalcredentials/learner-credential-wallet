b48fce936d752436a58c65fbfa8faad3ad040ade
kylehovey <khoveyc@gmail.com>
2021-09-01 17:59:50 -0600

    Removed unused screen

--- app/screens/ShareCredentialPreviewScreen/ShareCredentialPreviewScreen.d.ts
+++
@@ -1,1 +0,0 @@
-export type { ShareCredentialPreviewScreenProps } from '../../navigation';
--- app/screens/index.ts
+++ app/screens/index.ts
@@ -5,4 +5,3 @@ export { default as LoginScreen } from './LoginScreen/LoginScreen';
 export { default as QRScreen } from './QRScreen/QRScreen';
 export { default as ShareHomeScreen } from './ShareHomeScreen/ShareHomeScreen';
 export { default as PresentationPreviewScreen } from './PresentationPreviewScreen/PresentationPreviewScreen';
-export { default as ShareCredentialPreviewScreen } from './ShareCredentialPreviewScreen/ShareCredentialPreviewScreen';
