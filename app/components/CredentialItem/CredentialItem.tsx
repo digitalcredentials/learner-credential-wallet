197eb7c15c1ebce8596978940bb25f445b47bed1
kylehovey <khoveyc@gmail.com>
2021-09-01 11:38:19 -0600

    Removed unused import

--- app/components/CredentialItem/CredentialItem.tsx
+++ app/components/CredentialItem/CredentialItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, View, Text } from 'react-native';
+import { Image, View } from 'react-native';
 import { ListItem, CheckBox } from 'react-native-elements';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
