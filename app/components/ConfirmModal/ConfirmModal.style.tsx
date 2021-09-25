1ba2431d8557f01cb01e118c0635b3e027e2ec55
kylehovey <khoveyc@gmail.com>
2021-09-24 17:38:11 -0700

    Restoring accidentally deleted file

---
+++ app/components/ConfirmModal/ConfirmModal.style.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+import { theme, mixins } from '../../styles';
+
+export default StyleSheet.create({
+  modalBackground: {
+    ...StyleSheet.absoluteFillObject,
+    opacity: 0.75,
+    backgroundColor: theme.color.backgroundSecondary,
+  },
+  modalOuterContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  modalContainer: {
+    backgroundColor: theme.color.foregroundPrimary,
+    padding: 16,
+    alignItems: 'center',
+    borderRadius: theme.borderRadius,
+  },
+  modalTitle: {
+    color: theme.color.textPrimary,
+    fontFamily: theme.fontFamily.bold,
+    fontSize: theme.fontSize.regular,
+    marginVertical: 8,
+  },
+  buttonGroupContainer: {
+    marginTop: 16,
+  },
+  buttonPrimary: {
+    ...mixins.button, 
+    ...mixins.buttonPrimary,
+  },
+  buttonSecondary: {
+    ...mixins.button,
+    backgroundColor: theme.color.buttonSecondary,
+  },
+  buttonSecondaryTitle: {
+    ...mixins.buttonTitle,
+    color: theme.color.textPrimary,
+  },
+});
