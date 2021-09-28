6d6fc973d50f3c4f53dc9b7c540b04ca69803052
kylehovey <khoveyc@gmail.com>
2021-09-28 10:36:43 -0700

    Added error messages for QR scanner

--- app/screens/QRScreen/QRScreen.tsx
+++ app/screens/QRScreen/QRScreen.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { View } from 'react-native';
 import { Text } from 'react-native-elements';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { BarCodeReadEvent } from 'react-native-camera';
 
+import { ConfirmModal } from '../../components';
 import { stageCredentials } from '../../store/slices/credentialFoyer';
-import { credentialsFromQrText } from '../../lib/decode';
+import { credentialsFromQrText, isVpqr } from '../../lib/decode';
 import { NavHeader } from '../../components';
 import { QRScreenProps } from './QRScreen.d';
 import styles from './QRScreen.styles';
 
 export default function QRScreen({ navigation }: QRScreenProps): JSX.Element {
+  const [errorModalOpen, setErrorModalOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   const dispatch = useDispatch();
 
   function Instructions(): JSX.Element {
@@ -22,11 +25,24 @@ export default function QRScreen({ navigation }: QRScreenProps): JSX.Element {
     );
   }
 
-  async function onRead(e: BarCodeReadEvent) {
-    const credentials = await credentialsFromQrText(e.data);
-    dispatch(stageCredentials(credentials));
+  async function onRead({ data: text }: BarCodeReadEvent) {
+    if (!isVpqr(text)) {
+      setErrorModalOpen(true);
+      setErrorMessage('The QR code was read, but no credentials were found');
 
-    navigation.navigate('ApproveCredentialsScreen');
+      return;
+    }
+
+    await credentialsFromQrText(text)
+      .then((credentials) => {
+        dispatch(stageCredentials(credentials));
+        navigation.navigate('ApproveCredentialsScreen');
+      })
+      .catch((err) => {
+        console.warn(err);
+        setErrorModalOpen(true);
+        setErrorMessage('The QR code could not be parsed or is malformed');
+      });
   }
 
   return (
@@ -40,6 +56,14 @@ export default function QRScreen({ navigation }: QRScreenProps): JSX.Element {
         cameraStyle={styles.cameraStyle}
         markerStyle={styles.markerStyle}
         showMarker
+      />
+      <ConfirmModal
+        open={errorModalOpen}
+        onRequestClose={() => setErrorModalOpen(!errorModalOpen)}
+        onConfirm={() => navigation.goBack()}
+        confirmText="Okay"
+        cancelButton={false}
+        title={errorMessage}
       />
     </View>
   );
