2fcdef37c76d2b4e5f650cb40e2c42d8b26861bd
kylehovey <khoveyc@gmail.com>
2021-10-05 18:04:32 -0700

    Removed unused import

--- app/screens/ApproveCredentialScreen/ApproveCredentialScreen.tsx
+++ app/screens/ApproveCredentialScreen/ApproveCredentialScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView, Text } from 'react-native';
+import { View, ScrollView } from 'react-native';
 
 import { CredentialCard, VerificationCard } from '../../components';
 import { NavHeader, ApprovalControls } from '../../components';
