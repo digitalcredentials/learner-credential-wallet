e30b5a60130fe3e0ca00dbca5fead81487c11597
kylehovey <khoveyc@gmail.com>
2021-10-05 19:19:10 -0700

    Removed the "Restore from a file" button for adding credentials

--- app/screens/AddScreen/AddScreen.tsx
+++ app/screens/AddScreen/AddScreen.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Header, Text, Button } from 'react-native-elements';
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { theme, mixins } from '../../styles';
 import styles from './AddScreen.styles';
 import { AddScreenProps } from './AddScreen.d';
-import { navigationRef } from '../../../App';
 
 export default function AddScreen({ navigation }: AddScreenProps): JSX.Element {
-  function goToImport() {
-    if (navigationRef.isReady()) {
-      navigationRef.navigate('SettingsNavigation', {
-        screen: 'Restore',
-        initial: false,
-      });
-    }
-  }
-
   return (
     <>
       <Header
@@ -39,21 +29,6 @@ export default function AddScreen({ navigation }: AddScreenProps): JSX.Element {
           icon={
             <MaterialIcons
               name="qr-code-scanner"
-              size={theme.iconSize}
-              color={theme.color.iconInactive}
-            />
-          }
-        />
-        <Button
-          title="Restore from a file"
-          buttonStyle={mixins.buttonIcon}
-          containerStyle={mixins.buttonIconContainer}
-          titleStyle={mixins.buttonIconTitle}
-          iconRight
-          onPress={goToImport}
-          icon={
-            <MaterialCommunityIcons
-              name="file-upload"
               size={theme.iconSize}
               color={theme.color.iconInactive}
             />
