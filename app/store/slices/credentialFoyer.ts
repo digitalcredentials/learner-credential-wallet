2a1479e01ec86ff3a0f9da606c3c4474894741bf
Seth <sethduffin@gmail.com>
2021-11-08 18:02:05 -0700

    Added duplicate credential handling

--- app/store/slices/credentialFoyer.ts
+++ app/store/slices/credentialFoyer.ts
@@ -1,22 +1,38 @@
 import uuid from 'react-native-uuid';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 
+import { CredentialRecord } from '../../model';
 import type { Credential } from '../../types/credential';
 
 export enum ApprovalStatus {
+  Pending,
+  Accepted,
+  Rejected,
+  Errored,
+}
+
+export enum ApprovalMessage {
   Pending = 'None',
   Accepted = 'Added to Wallet',
   Rejected = 'Credential Declined',
   Errored = 'Credential Failed to Add',
+  Duplicate = 'Already in Wallet',
 }
 
 export class PendingCredential {
   id: string = uuid.v4() as string;
-  status: ApprovalStatus = ApprovalStatus.Pending;
+  status: ApprovalStatus;
   credential: Credential;
+  messageOveride?: ApprovalMessage;
 
-  constructor(credential: Credential) {
+  constructor(
+    credential: Credential, 
+    status: ApprovalStatus = ApprovalStatus.Pending, 
+    messageOveride?: ApprovalMessage,
+  ) {
     this.credential = credential;
+    this.status = status;
+    this.messageOveride = messageOveride;
   }
 }
 
@@ -28,17 +44,31 @@ const initialState: CredentialFoyerState = {
   pendingCredentials: [],
 };
 
+const stageCredentials = createAsyncThunk('credentialFoyer/stageCredentials', async (credentials: Credential[]) => {
+  const existingCredentials = await CredentialRecord.getAllCredentials();
+  const existingCredentialIds = existingCredentials.map(({ credential }) => credential.id);
+  
+  const pendingCredentials = credentials.map((credential) => {
+    if (existingCredentialIds.includes(credential.id)) {
+      return new PendingCredential(
+        credential, 
+        ApprovalStatus.Accepted, 
+        ApprovalMessage.Duplicate,
+      );
+    }
+
+    return new PendingCredential(credential);
+  });
+
+  return { pendingCredentials };
+});
+
 const credentialFoyer = createSlice({
   name: 'credentialFoyer',
   initialState,
   reducers: {
     clearFoyer(state) {
       state.pendingCredentials = initialState.pendingCredentials;
-    },
-    stageCredentials(state, action: PayloadAction<Credential[]>) {
-      state.pendingCredentials = action.payload.map((credential) => (
-        new PendingCredential(credential)
-      ));
     },
     setCredentialApproval(state, action: PayloadAction<PendingCredential>) {
       const isSubject = ({ id: given }: PendingCredential) => given === action.payload.id;
@@ -63,7 +93,14 @@ const credentialFoyer = createSlice({
       };
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(stageCredentials.fulfilled, (state, action) => ({
+      ...state,
+      ...action.payload,
+    }));
+  },
 });
 
 export default credentialFoyer.reducer;
-export const { clearFoyer, stageCredentials, setCredentialApproval } = credentialFoyer.actions;
+export const { clearFoyer, setCredentialApproval } = credentialFoyer.actions;
+export { stageCredentials };
