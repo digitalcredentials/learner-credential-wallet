3eaa175682a94a78e75749833b97d4483e7af007
Seth <sethduffin@gmail.com>
2021-11-23 00:07:44 -0700

    Added custom useAccessibilityFocus hook

---
+++ app/hooks/useAccessibilityFocus.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState, useRef, useMemo, Ref } from 'react';
+import { findNodeHandle, AccessibilityInfo } from 'react-native';
+
+export function useAccessibilityFocus<T>(): [Ref<T>, () => void] {
+  const [doFocus, setDoFocus] = useState(false);
+  const ref = useRef(null);
+  const reactTag = useMemo(() => findNodeHandle(ref?.current), [ref.current]);
+
+  useEffect(() => {
+    if (doFocus && ref.current !== null && reactTag !== null) {
+      AccessibilityInfo.setAccessibilityFocus(reactTag);
+      setDoFocus(false);
+    }
+
+    return () => setDoFocus(false);
+  }, [doFocus, ref]);
+
+  function focus(): void {
+    setDoFocus(true);
+  }
+
+  return [ref, focus];
+}
