7301d597e3eb8fcde79cb4f2f1623205ed27a085
Seth <sethduffin@gmail.com>
2022-01-07 12:51:32 -0700

    Added credential response error handling

--- app/lib/request.ts
+++ app/lib/request.ts
@@ -37,7 +37,7 @@ export async function requestCredential(credentialRequestParams: CredentialReque
 
   const { accessToken } = await authorize(config).catch((err) => {
     console.error(err);
-    throw Error('Unable to receive credential');
+    throw Error('Unable to receive credential:\nAuthorization failed');
   });
 
   const requestBody = await createVerifiablePresentation([], didRecord, challenge);
@@ -51,6 +51,12 @@ export async function requestCredential(credentialRequestParams: CredentialReque
   };
 
   const response = await fetch(vc_request_url, request);
+
+  if (!response.ok) {
+    console.error(`Issuer response (failed): ${JSON.stringify(response, null, 2)}`);
+    throw Error('Unable to receive credential:\nThe issuer failed to return a valid response');
+  }
+
   const responseJson  = await response.json();
   const credential = responseJson as Credential;
 
