04cc178af64b6bdf0e57e52dddace8ee639d17b3
Seth <sethduffin@gmail.com>
2022-01-19 20:04:17 -0700

    Added verification status screen

--- app/navigation/RootNavigation/RootNavigation.d.tsx
+++ app/navigation/RootNavigation/RootNavigation.d.tsx
@@ -1,6 +1,7 @@
 import type { NavigatorScreenParams } from '@react-navigation/native';
 import type { StackScreenProps } from '@react-navigation/stack';
 import type { CredentialRecordRaw } from '../../model/credential';
+import type { VerifyPayload } from '../../hooks';
 import type { HomeNavigationParamList } from '../';
 
 export type RootNavigationParamsList = {
@@ -8,7 +9,11 @@ export type RootNavigationParamsList = {
   DebugScreen: {
     rawCredentialRecord: CredentialRecordRaw;
   };
+  VerificationStatusScreen: {
+    verifyPayload: VerifyPayload;
+  };
 };
 
 export type HomeNavigationProps = StackScreenProps<RootNavigationParamsList, 'HomeNavigation'>
 export type DebugScreenProps = StackScreenProps<RootNavigationParamsList, 'DebugScreen'>
+export type VerificationStatusScreenProps = StackScreenProps<RootNavigationParamsList, 'VerificationStatusScreen'>
---
+++ app/screens/VerificationStatusScreen/VerificationStatusScreen.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+import { NavHeader, VerificationCard, VerificationStatusCard } from '../../components';
+
+import type { VerificationStatusScreenProps } from './VerificationStatusScreen.d';
+import styles from './VerificationStatusScreen.styles';
+
+export default function VerificationStatusScreen({
+  navigation,
+  route,
+}: VerificationStatusScreenProps): JSX.Element {
+  const { verifyPayload } = route.params;
+
+  return (
+    <>
+      <NavHeader
+        title="Verification Status"
+        goBack={() => navigation.goBack()}
+      />
+      <ScrollView style={styles.container}>
+        <VerificationCard verifyPayload={verifyPayload} />
+        <VerificationStatusCard verifyPayload={verifyPayload} />
+        <Text style={styles.footerText}>
+          Please contact your issuing organization if you face any problems with
+          the verification status.
+        </Text>
+      </ScrollView>
+    </>
+  );
+}
