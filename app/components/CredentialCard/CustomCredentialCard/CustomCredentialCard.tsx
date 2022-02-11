6e8b04077a1e6b39e20951741f16475c90d14d62
Dirk Uys <dirkcuys@gmail.com>
2022-02-11 13:38:08 +0200

    Restructure custom crediential cards

--- app/components/CredentialCard/CustomCredentialCard/CustomCredentialCard.tsx
+++ app/components/CredentialCard/CustomCredentialCard/CustomCredentialCard.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import type { CredentialRecordRaw } from '../../model/credential';
-import styles from './CustomCredentialCard.styles';
-
-type CredentialCardProps = {
-  rawCredentialRecord: CredentialRecordRaw;
-};
+import styles from '../CredentialCard.styles';
+import type { CredentialCardProps } from '../CredentialCard.d';
 
 
 export default function CustomCredentialCard ({ rawCredentialRecord }: CredentialCardProps): JSX.Element {
