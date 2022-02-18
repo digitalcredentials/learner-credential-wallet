1396543c487823429b999ddaa27582107ae206ac
Dirk Uys <dirkcuys@gmail.com>
2022-02-18 13:35:05 +0200

    Update CredentialCard to display StudentIdCard.

--- app/components/CredentialCard/CredentialCard.tsx
+++ app/components/CredentialCard/CredentialCard.tsx
@@ -6,6 +6,7 @@ import type { CredentialRecordRaw } from '../../model/credential';
 import DefaultCredentialCard from './DefaultCredentialCard/DefaultCredentialCard';
 import CustomCredentialCard from './CustomCredentialCard/CustomCredentialCard';
 import UniversityDegreeCredentialCard from './UniversityDegreeCredentialCard/UniversityDegreeCredentialCard';
+import StudentIdCard from './StudentIdCard';
 
 import type { CredentialCardProps } from './CredentialCard.d';
 
@@ -14,22 +15,41 @@ export default function CredentialCard({ rawCredentialRecord }: CredentialCardPr
   const { credential } = rawCredentialRecord;
   const { credentialSubject } = credential;
 
+  // Possible way to handle component + title
+  // const credentialTypes = [
+  //   {
+  //     match: rawCredential => {
+  //     },
+  //     component: StudentIdCard,
+  //     renderTitle: rawCredential => {
+  //     }
+  //   }
+  // ];
+  //
+  //
+  // for (credType of credentialTypes){
+  //   if (credType.match(rawCredential)){
+  //       return credType.component(rawCredential)
+  //       // or in CredentialItem:
+  //       // return credType.renderTitle(rawCredential)
+  //   }
+  // }
+
   if ( credential.type.includes('UniversityDegreeCredential') ){
-    return <UniversityDegreeCredentialCard rawCredentialRecord={rawCredentialRecord} />
+    return <UniversityDegreeCredentialCard rawCredentialRecord={rawCredentialRecord} />;
   }
 
-  // TODO check credential.type contains 'assertion'
-
-  // Probably need to check that hasCredential and hasCredential.type is not null/undefined
-
   if (
-    credentialSubject.hasCredential && credentialSubject.hasCredential.type &&
+    credentialSubject.hasCredential && 
+    credentialSubject.hasCredential.type &&
     credentialSubject.hasCredential.type.includes('ProgramCompletionCredential')){
     return CustomCredentialCard({rawCredentialRecord});
   }
 
+  if (credential.type.includes('StudentId')){
+    return <StudentIdCard rawCredentialRecord={rawCredentialRecord} />;
+  }
+
   // maybe rename to generic credential card
-  return (
-    <DefaultCredentialCard rawCredentialRecord={rawCredentialRecord} />
-  );
+  return <DefaultCredentialCard rawCredentialRecord={rawCredentialRecord} />;
 }
