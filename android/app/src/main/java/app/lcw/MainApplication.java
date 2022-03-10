bd7b5030be187a10211dff876eb508d7876bf2b3
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-03-10 18:08:35 -0500

    Change android & ios app id to app.lcw.

--- android/app/src/main/java/app/lcw/MainApplication.java
+++ android/app/src/main/java/app/lcw/MainApplication.java
@@ -1,4 +1,4 @@
-package edu.wallet;
+package app.lcw;
 
 import android.app.Application;
 import android.content.Context;
@@ -12,7 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import edu.wallet.generated.BasePackageList;
+import app.lcw.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -100,7 +100,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("edu.wallet.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("app.lcw.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
