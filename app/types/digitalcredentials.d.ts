7c2ebf4156a034ef8924d7c01d254aa81c02d498
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-05-25 15:05:37 -0400

    Add support for OpenBadges v3 context.

--- app/types/digitalcredentials.d.ts
+++ app/types/digitalcredentials.d.ts
@@ -6,6 +6,7 @@ declare module '@digitalcredentials/vpqr';
 declare module '@digitalcredentials/ed25519-verification-key-2020';
 declare module '@digitalcredentials/ed25519-signature-2020';
 declare module '@digitalcredentials/jsonld-signatures';
+declare module '@digitalcredentials/open-badges-context';
 declare module '@digitalcredentials/x25519-key-agreement-key-2020';
 declare module 'credentials-context';
 declare module 'crypto-ld';
