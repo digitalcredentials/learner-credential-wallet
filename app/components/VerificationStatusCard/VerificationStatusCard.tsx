a7c2b84860c2bbbe689cbc3798186081e9e04e87
kezike <kezike13@gmail.com>
2022-08-14 13:59:36 -0400

    fixed issue fetching status list; made minor copy change

--- app/components/VerificationStatusCard/VerificationStatusCard.tsx
+++ app/components/VerificationStatusCard/VerificationStatusCard.tsx
@@ -63,8 +63,8 @@ export default function VerificationStatusCard({ credential, verifyPayload }: Ve
         verified={details[LogId.IssuerDIDResolves]}
       />
       <StatusItem
-        positiveText="Has not been revoked"
-        negativeText="Has been revoked"
+        positiveText="Has not been revoked by issuer"
+        negativeText="Has been revoked by issuer"
         verified={details[LogId.RevocationStatus]}
       />
       <StatusItem
