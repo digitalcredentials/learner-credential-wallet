139f25de11b672bd7ddb0dd585dda85bfb5f8353
Seth <sethduffin@gmail.com>
2022-09-16 11:22:17 -0600

    Added issuer info button

--- app/components/CredentialCard/CredentialCard.d.ts
+++ app/components/CredentialCard/CredentialCard.d.ts
@@ -2,6 +2,7 @@ import type { CredentialRecordRaw } from '../../model/credential';
 
 export type CredentialCardProps = {
   rawCredentialRecord: CredentialRecordRaw,
+  onPressIssuer: (issuerId: string) => void,
 }
 
 export type CredentialRenderInfo = {
