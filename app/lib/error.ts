0508f7813da34dd687d9067a924de774188f6176
Seth <sethduffin@gmail.com>
2022-10-03 13:08:44 -0600

    Fixed human readable error identifier

--- app/lib/error.ts
+++ app/lib/error.ts
@@ -1,15 +1,16 @@
 const FALLBACK_ERROR_MESSAGE = 'Something went wrong';
+const HUMAN_READABLE_ERROR_NAME = 'HumanReadableError';
 
 export class HumanReadableError extends Error {
   constructor(message: string) {
     super(message);
     this.message = message;
-    this.name = 'HumanReadableError';
+    this.name = HUMAN_READABLE_ERROR_NAME;
   }
 }
 
 function isHumanReadableError(err: unknown): err is HumanReadableError {
-  return (err as Error).name === HumanReadableError.name;
+  return (err as Error).name === HUMAN_READABLE_ERROR_NAME;
 }
 
 export function errorMessageFrom(err: unknown, fallback = FALLBACK_ERROR_MESSAGE): string {
