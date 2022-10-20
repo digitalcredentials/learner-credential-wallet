153589a19bd0e4d77d9352a8864172fb351f4503
Seth <sethduffin@gmail.com>
2022-10-20 13:14:35 -0600

    Added global error interface

--- app/navigation/RootNavigation/RootNavigation.tsx
+++ app/navigation/RootNavigation/RootNavigation.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { DebugScreen, VerificationStatusScreen } from '../../screens';
 import {
   HomeNavigation,
   RootNavigationParamsList,
-  AcceptCredentialsNavigation
+  AcceptCredentialsNavigation,
 } from '../../navigation';
+import { ConfirmModal } from '../../components';
+import { useDynamicStyles } from '../../hooks';
+import { clearGlobalError, selectWalletState } from '../../store/slices/wallet';
 
 const Stack = createStackNavigator<RootNavigationParamsList>();
 
 export default function RootNavigation(): JSX.Element {
+  const dispatch = useDispatch();
+  const { mixins } = useDynamicStyles();
+  const { globalError } = useSelector(selectWalletState);
+  const isModalOpen = globalError !== null;
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="HomeNavigation" component={HomeNavigation} />
-      <Stack.Screen name="DebugScreen" component={DebugScreen} />
-      <Stack.Screen name="VerificationStatusScreen" component={VerificationStatusScreen} />
-      <Stack.Screen name="AcceptCredentialsNavigation" component={AcceptCredentialsNavigation} />
-    </Stack.Navigator>
+    <>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="HomeNavigation" component={HomeNavigation} />
+        <Stack.Screen name="DebugScreen" component={DebugScreen} />
+        <Stack.Screen name="VerificationStatusScreen" component={VerificationStatusScreen} />
+        <Stack.Screen name="AcceptCredentialsNavigation" component={AcceptCredentialsNavigation} />
+      </Stack.Navigator>
+      <ConfirmModal
+        open={isModalOpen}
+        onRequestClose={() => dispatch(clearGlobalError())}
+        title={globalError?.title}
+        cancelButton={false}
+        confirmText="Close"
+        cancelOnBackgroundPress
+      >
+        <Text style={mixins.modalBodyText}>{globalError?.message}</Text>
+      </ConfirmModal>
+    </>
   );
 }
