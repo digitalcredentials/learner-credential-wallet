444133cb49e1ac5984ebc3e9b1287feacf8c6b2f
Seth <sethduffin@gmail.com>
2022-10-25 16:09:42 -0600

    Updated developer functions

--- app/screens/DeveloperScreen/DeveloperScreen.tsx
+++ app/screens/DeveloperScreen/DeveloperScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Linking, View } from 'react-native';
 import { Button } from 'react-native-elements';
 
 import { NavHeader } from '../../components';
@@ -10,18 +10,31 @@ import { credentials } from '../../mock/credential';
 import { navigationRef } from '../../navigation';
 import { Cache, CacheKey } from '../../lib/cache';
 import { useAppDispatch, useDynamicStyles } from '../../hooks';
+import { revokedCredential } from '../../mock/revokedCredential';
 
 export default function DeveloperScreen({ navigation }: DeveloperScreenProps): JSX.Element {
   const { styles, mixins } = useDynamicStyles(dynamicStyleSheet);
   const dispatch = useAppDispatch();
 
-  async function addMockCredential() {
-    await dispatch(stageCredentials(credentials));
+  function goToChooseProfile() {
     if (navigationRef.isReady()) {
       navigationRef.navigate('AcceptCredentialsNavigation', { 
         screen: 'ChooseProfileScreen',
       });
     }
+  }
+
+  async function addMockCredentials() {
+    await dispatch(stageCredentials(credentials));
+    goToChooseProfile();
+  }
+
+  async function addRevokedCredential() {
+    await dispatch(stageCredentials([revokedCredential]));
+  }
+
+  function receiveCredentialThroughDeepLink() {
+    Linking.openURL('dccrequest://request?issuer=https%3A%2F%2Fissuer.example.com&vc_request_url=https://verify.dcconsortium.org/request/credential&challenge=ke12345678-0001&auth_type=bearer');
   }
 
   async function clearVerificationCache(): Promise<void> {
@@ -38,7 +51,21 @@ export default function DeveloperScreen({ navigation }: DeveloperScreenProps): J
           buttonStyle={mixins.buttonIconCompact}
           containerStyle={mixins.buttonContainerVertical}
           titleStyle={mixins.buttonIconTitle}
-          onPress={addMockCredential}
+          onPress={addMockCredentials}
+        />
+        <Button
+          title="Add revoked credential"
+          buttonStyle={mixins.buttonIconCompact}
+          containerStyle={mixins.buttonContainerVertical}
+          titleStyle={mixins.buttonIconTitle}
+          onPress={addRevokedCredential}
+        />
+        <Button
+          title="Receive credential through deep link"
+          buttonStyle={mixins.buttonIconCompact}
+          containerStyle={mixins.buttonContainerVertical}
+          titleStyle={mixins.buttonIconTitle}
+          onPress={receiveCredentialThroughDeepLink}
         />
         <Button
           title="Clear verification cache"
