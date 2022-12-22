ba629aa0164b77b4a82c5fcdf1ee9a3db34c254b
Seth <sethduffin@gmail.com>
2022-12-21 17:44:27 -0700

    Resolve backlogged runtime warnings

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import { LogBox } from 'react-native';
 
 import store from './app/store';
 import { AppNavigation } from './app/navigation';
 import { ThemeProvider } from './app/components';
+
+LogBox.ignoreLogs([
+  'Non-serializable values were found in the navigation state',
+]);
 
 export default function App(): JSX.Element {
   return (
