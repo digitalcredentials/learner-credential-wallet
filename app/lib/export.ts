13f458b77b3248b917c73099f1ec2fda027b0883
Jenna Cioffi <Jenna.cioffi@atomicjolt.com>
2023-08-10 13:18:38 -0600

    fixing lint warnings and errors

--- app/lib/export.ts
+++ app/lib/export.ts
@@ -1,28 +1,19 @@
 import { ProfileRecord, ProfileRecordRaw } from '../model';
-// import { exportWalletEncrypted } from './lockedWallet';
 import { shareData } from './shareData';
 
-export async function exportProfile(rawProfileRecord: ProfileRecordRaw, encryptPassphrase?: string): Promise<void> {
+export async function exportProfile(rawProfileRecord: ProfileRecordRaw): Promise<void> {
   const exportedProfile = await ProfileRecord.exportProfileRecord(rawProfileRecord);
   const exportedProfileString = JSON.stringify([exportedProfile], null, 2);
-
-  // const data = encryptPassphrase
-  //   ? await exportWalletEncrypted(exportedProfileString, encryptPassphrase)
-  //   : exportedProfileString;
 
   const data = exportedProfileString;
 
   await shareData('Profile Backup.json', data);
 }
 
-export async function exportWallet(encryptPassphrase?: string): Promise<void> {
+export async function exportWallet(): Promise<void> {
   const rawProfileRecords = await ProfileRecord.getAllProfileRecords();
   const exportedProfiles = await Promise.all(rawProfileRecords.map(ProfileRecord.exportProfileRecord));
   const exportedWalletString = JSON.stringify(exportedProfiles, null, 2);
-
-  // const data = encryptPassphrase
-  //   ? await exportWalletEncrypted(exportedWalletString, encryptPassphrase)
-  //   : exportedWalletString;
 
   const data = exportedWalletString;
 
