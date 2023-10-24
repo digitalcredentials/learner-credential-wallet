924bba9e0305484ca55ad8fb60727797c042afda
Dmitri Zagidulin <dzagidulin@gmail.com>
2023-10-24 18:45:42 -0400

    Associate LCW with lcw.app domain.

--- app/lib/registerWallet.ts
+++ app/lib/registerWallet.ts
@@ -1,9 +1,9 @@
 import {Linking} from 'react-native';
 
 export function registerWallet(): void {
-  Linking.canOpenURL('https://lcw.app/register-wallet').then(supported => {
+  Linking.canOpenURL('https://lcw.app/register-wallet.html').then(supported => {
     if (supported) {
-      Linking.openURL('https://lcw.app/register-wallet');
+      Linking.openURL('https://lcw.app/register-wallet.html');
     } else {
       console.log('Unable to register wallet! Failed URL');
     }
