e5db8bddf3b49f94d21e47a1c68c87b05dc0d9f9
Vojtech Novak <vonovak@gmail.com>
2024-01-15 22:03:32 +0100

    feat: remove encodeQueryParams2

--- app/lib/encode.unit.test.ts
+++ app/lib/encode.unit.test.ts
@@ -1,6 +1,8 @@
-import {encodeQueryParams, encodeQueryParams2} from './encode';
+import {encodeQueryParams, } from './encode';
 
 describe('encodeQueryParams', () => {
+  // note this was added after encodeQueryParams was written, and it's not set in stone that
+  // this is the desired behavior. These tests are more for documenting the current behavior.
   it.each([
     // Test case format: [description, input, expectedOutput]
     ['No reserved characters', 'param1=value1&param2=value2', 'param1=value1&param2=value2'],
@@ -9,12 +11,10 @@ describe('encodeQueryParams', () => {
     ['Empty string', '', ''],
     ['String without \'=\'', 'justastring', 'justastring'],
     ['Only reserved characters', 'param1=;,/?:@&=+$', 'param1=%3B%2C%2F%3F%3A%40&=%2B%24'],
-    // TODO - for this case, which is the expected output? The one here in the test case or the one from encodeQueryParams?
-    // ['Spaces and special characters',
-    //   'param1=space test&param2=odd@char&param3=100%match',
-    //   'param1=space%20test&param2=odd%40char&param3=100%25match'],
+    ['Spaces and special characters',
+      'param1=space test&param2=odd@char&param3=100%match',
+      'param1=space test&param2=odd%40char&param3=100%match'],
   ])('%s', (_description, input, expected) => {
-    expect(encodeQueryParams2(input)).toBe(expected);
     expect(encodeQueryParams(input)).toBe(expected);
   });
 });
