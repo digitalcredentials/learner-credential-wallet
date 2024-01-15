78dd4cb08cfd7cfea826bf352d190b69344a4e59
Vojtech Novak <vonovak@gmail.com>
2024-01-15 21:26:55 +0100

    feat: add jest and a simple test

--- app/lib/encode.ts
+++ app/lib/encode.ts
@@ -2,17 +2,21 @@ const PARAM_REGEX = /=([^&]+)/g;
 const RESERVED_REGEX = /[;,/?:@&=+$]/g;
 
 /**
- * Utility method for encoding url query params containing reserved 
+ * Utility method for encoding url query params containing reserved
  * characters.
- */ 
+ */
 export function encodeQueryParams (url: string): string {
-
   return url.replace(PARAM_REGEX, (_, value) => {
     if (value.match(RESERVED_REGEX)) {
       return `=${encodeURIComponent(value)}`;
     } else {
       return `=${value}`;
     }
+  });
+}
+export function encodeQueryParams2 (url: string): string {
+  return url.replace(PARAM_REGEX, (_, value) => {
+    return `=${encodeURIComponent(value)}`;
   });
 }
 
