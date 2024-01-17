19803b4c1d770dbf7b3450c9f1c46052acd3b971
Vojtech Novak <vonovak@gmail.com>
2024-01-17 22:33:12 +0100

    chore: update deps

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+// eslint-disable-next-line no-undef
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            'crypto': 'react-native-quick-crypto',
+            'stream': 'stream-browserify',
+            'buffer': '@craftzdog/react-native-buffer',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
