8ef3f8a9d8928f8d4d586b4c9747e0369fe00ef8
Vojtech Novak <vonovak@gmail.com>
2024-02-23 17:35:11 +0100

    fix: import correct @digitalbazaar/http-client dep

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 
 defaultConfig.resolver.assetExts.push('cjs');
+// this will be the default in the future, so let's be future-proof
+defaultConfig.resolver.enablePackageExports = true;
+defaultConfig.resolver.unstable_enablePackageExports = true;
 
 module.exports = defaultConfig;
