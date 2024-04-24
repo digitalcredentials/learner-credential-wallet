b7451c0a694c2c5a4cd946a3011b08b978c80ece
Dmitri Zagidulin <dzagidulin@gmail.com>
2024-04-24 15:00:14 -0400

    Attempt to open Register Wallet page regardless of canOpenURL.

--- app/lib/registerWallet.ts
+++ app/lib/registerWallet.ts
@@ -1,12 +1,12 @@
 import { Linking } from 'react-native';
 import { LinkConfig } from '../config';
 
-export function registerWallet(): void {
-  Linking.canOpenURL(LinkConfig.registerWalletUrl).then(supported => {
-    if (supported) {
-      Linking.openURL(LinkConfig.registerWalletUrl);
-    } else {
-      console.log('Unable to register wallet! Failed URL');
-    }
-  });
+export async function registerWallet(): void {
+  Linking.openURL(LinkConfig.registerWalletUrl);
+
+  // Issue a warning in case canOpenURL() is reporting as false
+  // See RN issue #32311 however -- this can be a false signal.
+  if(!(await Linking.canOpenURL(LinkConfig.registerWalletUrl))) {
+    console.log(`WARNING: Opening browser to register wallet, Linking.canOpenURL(${LinkConfig.registerWalletUrl}) is reporting 'false'.`)
+  }
 }
