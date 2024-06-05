4cbdba22cf736d3af3040d454b3fe49e4cb4a142
Dmitri Zagidulin <dzagidulin@gmail.com>
2024-06-05 13:22:41 -0400

    Fix exchange chapi import.

--- app/navigation/ExchangeCredentialsNavigation/ExchangeCredentialsNavigation.d.ts
+++ app/navigation/ExchangeCredentialsNavigation/ExchangeCredentialsNavigation.d.ts
@@ -1,5 +1,5 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import { ChapiCredentialRequest } from '../../lib/chapi';
+import { ChapiCredentialRequest } from '../../types/chapi';
 
 export type ExchangeCredentialsNavigationParamList = {
   ExchangeCredentials: { request: ChapiCredentialRequest; };
