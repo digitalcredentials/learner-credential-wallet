a5c0a7439437ae02ad26656e8cab669fdadebf65
Dmitri Zagidulin <dzagidulin@gmail.com>
2024-06-20 17:17:38 -0400

    Drop unused disableFlipper script, update android version.

--- disableAndroidFlipper.sh
+++
@@ -1,6 +0,0 @@
-set -x -e
-# comments out flipper support for Android because it clashes with quick-crypto
-sed -i.bak '/implementation("com.facebook.react:flipper-integration")/s/^/\/\/ /' ./android/app/build.gradle
-
-sed -i.bak '/ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)/s/^/\/\/ /' ./android/app/src/main/java/edu/wallet/MainApplication.kt
-sed -i.bak '/import com.facebook.react.flipper.ReactNativeFlipper/s/^/\/\/ /' ./android/app/src/main/java/edu/wallet/MainApplication.kt
