e282eb8f358da7fa24bba7c4c6f74d69f4199a24
Dmitri Zagidulin <dzagidulin@gmail.com>
2024-09-17 23:57:54 -0400

    Fix VC verification (jsigs/expo-crypto related).

--- app/types/declarations.d.ts
+++ app/types/declarations.d.ts
@@ -4,7 +4,6 @@ declare module '@digitalcredentials/vc';
 declare module '@digitalcredentials/vc-bitstring-status-list';
 declare module '@digitalcredentials/vc-status-list';
 declare module '@digitalcredentials/vpqr';
-declare module '@digitalbazaar/http-client';
 declare module '@digitalcredentials/jsonld-signatures';
 declare module '@digitalcredentials/ed25519-signature-2020' {
   export class Ed25519Signature2020 {
